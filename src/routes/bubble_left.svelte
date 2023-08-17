31cdc9087dee2e0761bff0ff88a2cb2eff47c3e6
yunik1004 <inkyuhak@gmail.com>
2023-08-17 18:01:59 +0900

    Fix: Add 'base' into the image source
    - src: "{base}/xxx.png"

--- src/routes/bubble_left.svelte
+++ src/routes/bubble_left.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	export let content: string;
 </script>
 
 <div class="chat chat-start">
 	<div class="chat-image avatar">
 		<div class="w-10 rounded-full">
-			<img src="/turtle.png" class="invert" alt="ChatGPT profile" />
+			<img src="{base}/turtle.png" class="invert" alt="ChatGPT profile" />
 		</div>
 	</div>
 	<div class="chat-bubble chat-bubble-secondary">{content}</div>
