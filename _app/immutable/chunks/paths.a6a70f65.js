080225b461436431d4ac570a5171a955a59c0f28
yunik1004 <yunik1004@users.noreply.github.com>
2023-08-17 09:02:58 +0000

    deploy: 31cdc9087dee2e0761bff0ff88a2cb2eff47c3e6

--- _app/immutable/chunks/paths.a6a70f65.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1k7ezry)==null?void 0:s.base)??"/LTP-with-ChatGPT";var e;const a=((e=globalThis.__sveltekit_1k7ezry)==null?void 0:e.assets)??t;export{a,t as b};
